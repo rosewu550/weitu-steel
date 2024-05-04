62f63a210c5504a7ccca2ddf7c68763419ef5485
alphaxx <ww792115071@gmail.com>
2024-05-04 21:46:41 +0800

    perf(no.4) no.4 联系我们

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -4,28 +4,44 @@
     <div class="contact-us-container">
       <div class="map-container">
         <MapContainer class="map"></MapContainer>
-        <div class="contact-us-card">
-          <p class="card-title">WEITU STEEL</p>
-          <p class="card-sub-title">SHANGHAI WEITU STEEL STRUCTURE ENGINEERING CO., LTD.</p>
-          <div class="card-detail">
-            <img class="horizontal-divider" src="../assets/img/horizon-divider.png" alt="">
-            <div class="detail-title">
-              <div class="detail-word">address:</div>
+        <div class="sub-map-container">
+          <div class="contact-us-card">
+            <p class="card-title">WEITU STEEL</p>
+            <p class="card-sub-title">SHANGHAI WEITU STEEL STRUCTURE ENGINEERING CO., LTD.</p>
+            <div class="card-detail">
+              <img class="horizontal-divider" src="../assets/img/horizon-divider.png" alt="">
+              <div class="detail-title">
+                <div class="detail-word">address:</div>
+              </div>
+              <div class="detail-title">
+                <div class="detail-word address-detail">Room 902, Building 5, Lane 680, Shuichan West Road, Baoshan
+                  District, Shanghai, China
+                </div>
+              </div>
+
             </div>
-            <div class="detail-title">
-              <div class="detail-word address-detail">Room 902, Building 5, Lane 680, Shuichan West Road, Baoshan District, Shanghai, China</div>
+            <div class="card-detail">
+              <img class="horizontal-divider" src="../assets/img/horizon-divider.png" alt="">
+              <div class="detail-word">post code:</div>
+              <div class="detail-word">200126</div>
+            </div>
+          </div>
+          <div class="left-contact-us">
+            <div class="left-part left-telephone">
+              <img src="../assets/img/telephone.png" alt="">
+              <div class="left-part-title">telephone:</div>
+              <p>13091119111</p>
+            </div>
+            <div class="left-part left-email">
+              <img src="../assets/img/email.png" alt="">
+              <div class="left-part-title">email:</div>
+              <p>912112919191@qq.com</p>
             </div>
 
           </div>
-          <div class="card-detail">
-            <img class="horizontal-divider" src="../assets/img/horizon-divider.png" alt="">
-            <div class="detail-word">post code:</div>
-            <div class="detail-word">200126</div>
-          </div>
-
         </div>
-      </div>
 
+      </div>
     </div>
   </div>
 </template>
@@ -88,7 +104,7 @@ export default {
     overflow: hidden;
     border-radius: 5px;
 
-    .map{
+    .map {
       height: 500px;
     }
 
@@ -106,24 +122,25 @@ export default {
       padding: 40px 0 0 50px;
       color: #fff;
 
-      .card-title{
-        font-size: 25px;
+      .card-title {
+        font-size: 16px;
         font-weight: bold;
       }
 
       .card-sub-title {
-        font-size: 16px;
+        font-size: 20px;
         font-weight: bold;
         margin-top: 15px;
+        width: 90%;
 
       }
 
-      .card-detail{
+      .card-detail {
         display: flex;
         flex-direction: row;
         margin-top: 30px;
 
-        .horizontal-divider{
+        .horizontal-divider {
           background: white;
           height: 1px;
           position: relative;
@@ -140,13 +157,51 @@ export default {
           transform: translateY(-50%);
         }
 
-        .address-detail{
+        .address-detail {
           width: 70%;
           margin: 0 50px 0 23px;
         }
       }
     }
+
+    .sub-map-container {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: flex-start;
+
+      .left-contact-us {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        position: absolute;
+        float: left;
+        padding: 80px 0 20px 0;
+
+
+        .left-part {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          column-gap: 20px;
+          font-size: 20px;
+          padding: 0 0 40px 0;
+
+
+          .left-part-title{
+            font-size: 25px;
+            font-weight: bold;
+          }
+
+          img {
+            width: 60px;
+            height: 60px;
+          }
+        }
+      }
+    }
   }
 }
+
 
 </style>
